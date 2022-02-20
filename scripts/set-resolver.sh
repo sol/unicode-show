5b424c1267131f958a3954ad7b12ee57058b5d41
removedporn <86824510+removedporn@users.noreply.github.com>
2022-02-21 00:15:09 +0800

    Delete scripts directory

--- scripts/set-resolver.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-LATEST_VERSION=$(wget -O/dev/null https://www.stackage.org/$1 2>&1 |
-                     grep "https://www.stackage.org/$1" |
-                     tail -n1 |
-                     sed "s/.*$1/$1/")
-sed "s/^resolver: .*/resolver: ${LATEST_VERSION}/" stack.yaml
